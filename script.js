59d5d26c870f1e8f3242def6da8c31cf12e2590b
Pradip Sable <sablepradipsubhash@gmail.com>
2023-04-27 22:38:26 +0530

    bug fixed

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ let inputTypeValue, resultTypeValue;
 let convBtn = document.getElementById("convert");
 
 // add a eventlisteners
-convBtn.addEventListener("change", changeTemperature);
-
+convBtn.addEventListener("click", changeTemperature);
+inputTypeValue.addEventListener("change", changeTemperature);
+resultTypeValue.addEventListener("change", changeTemperature);
 
 function changeTemperature() {
   inputTypeValue = inputType.value;
@@ -16,11 +17,14 @@ function changeTemperature() {
   //Degree Celsius
   if (inputTypeValue === "Degree Celsius" && resultTypeValue === "Fahrenheit") {
     result.value = Number((input.value * 9) / 5) + 32;
+    // console.log(result.value); 
   } else if (
     inputTypeValue === "Degree Celsius" &&
     resultTypeValue === "Kelvin"
   ) {
-    result.value = Number((input.value) + 273.15);
+    // console.log(input.value);
+    result.value = Number(input.value) + 273.15;
+
   } else if (
     inputTypeValue === "Degree Celsius" &&
     resultTypeValue === "Degree Celsius"
@@ -32,7 +36,6 @@ function changeTemperature() {
     result.value = (Number(input.value - 32) * 5) / 9;
   } else if (inputTypeValue === "Fahrenheit" && resultTypeValue === "Kelvin") {
     result.value = (Number(input.value - 32) * 5) / 9 + 273.15;
-
   } else if (
     inputTypeValue === "Fahrenheit" &&
     resultTypeValue === "Fahrenheit"
@@ -43,7 +46,7 @@ function changeTemperature() {
   if (inputTypeValue === "Kelvin" && resultTypeValue === "Degree Celsius") {
     result.value = Number(input.value) - 273.15;
   } else if (inputTypeValue === "Kelvin" && resultTypeValue === "Fahrenheit") {
-    result.value = (Number(input.value - 273.15) * 9) / 5 + 32;
+    result.value = ((Number(input.value) - 273.15) * 9) / 5 + 32;
   } else if (inputTypeValue === "Kelvin" && resultTypeValue === "Kelvin") {
     result.value = input.value;
   }
